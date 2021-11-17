9f6660f9a0c3074a8c93408cbcf2a0a66e942dfd
Brennan McCandliss <osmedia1220@gmail.com>
2021-11-16 16:13:30 -0800

    added show password

--- client/src/Login.js
+++ client/src/Login.js
@@ -22,15 +22,23 @@ function Login() {
   //     });
   //   }
 
+  function toggleShowPassword(){
+    const password = document.querySelector('#password_id');
+      const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+      password.setAttribute('type', type);
+  }
+
   return (
     <div>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
       <h3>Username</h3>
       <form>
         <input type="text" />
       </form>
       <h3>Password</h3>
       <form>
-        <input type="password" minlength="8" required />
+        <input type="password" minlength="5" maxlength="15" id="password_id" required />
+        <i className="far fa-eye" id="togglePassword" onClick={toggleShowPassword} style={{marginLeft: "-25px", cursor: "pointer"}}/>
       </form>
       <button type="submit">
         Submit
